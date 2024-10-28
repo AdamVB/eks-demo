54256cbb9f57dec8642890a95b9343029c75334e
Adam Benesh <Adam.Benesh@lacework.net>
2024-10-28 13:17:37 +0100

    fix github action

--- application/variables.tf
+++ application/variables.tf
@@ -4,3 +4,9 @@
 variable "region" {
   default = "eu-west-1"
 }
+
+variable "example_api_key" {
+  description = "example api key coming from github secret"
+  type        = string
+  default     = "hello-world"
+}
